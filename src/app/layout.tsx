4c219b161bf1f89c9c2cb0b6823868abbb5f9d2f
Bassamnaeem <bassamnaeem01@gmail.com>
2026-05-12 13:36:50 +0500

    feat(tracking): add Facebook Pixel tracking script and noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Nunito, Nunito_Sans, Outfit, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import "../styles/marketing.css";
+
+const META_PIXEL_ID = "2654550048294305";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -48,6 +51,28 @@ export default function RootLayout({
       className={`${nunito.variable} ${nunitoSans.variable} ${outfit.variable} ${inter.variable} h-full`}
     >
       <body className="min-h-full flex flex-col font-sans text-navy bg-white antialiased">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
